f79863619575d2c6b2a6295d90677fe526f1840c
TrungXabi <thanhtrungnait88@gmail.com>
2018-01-10 17:41:20 +0700

    ffffff

---
+++ lethanhtrung_tuan1/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+			  $('.bxslider').bxSlider({
+			  auto: true,
+			  autoControls: true,
+			  stopAutoOnClick: true,
+			  pager: true,
+			  slideWidth: 600
+			});
+		});
