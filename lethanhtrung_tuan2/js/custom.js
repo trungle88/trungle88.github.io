51361f6f554bc2f7f8c9af7a9b301e562d5d7d09
Trung Xabi <thanhtrungnait88@gmail.com>
2018-01-17 11:53:53 +0700

    cffffff

---
+++ lethanhtrung_tuan2/js/custom.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+	$('.primary-nav-triggle').on('click', function(){
+		$('.menu-icon').toggleClass('is-clicked'); 
+		$('.header').toggleClass('menu-is-open');
+		
+		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+		if( $('.primary-nav').hasClass('is-visible') ) {
+			$('.primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').removeClass('overflow-hidden');
+			});
+		} else {
+			$('.primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').addClass('overflow-hidden');
+			});	
+		}
+	});
+});
