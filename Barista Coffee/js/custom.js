b041a6d34e71823de8362ca1ae6f9f7e422045d4
trung le <thanhtrungnait88@gmail.com>
2018-03-02 10:57:09 +0700

    fdfggfg

--- Barista Coffee/js/custom.js
+++ Barista Coffee/js/custom.js
@@ -19,3 +19,17 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function(){
+	$(window).scroll(function(){
+        if($(this).scrollTop() > 600){
+            $('.back-to-top').fadeIn();
+        }else{
+            $('.back-to-top').fadeOut();
+        }
+    });
+	$(".back-to-top").click(function () {
+	   $("html, body").animate({scrollTop: 0}, 1000);
+	});
+});
+
+
