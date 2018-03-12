053c111ae0d2deece1d3511a0e657fd6b40f4bf0
trung le <thanhtrungnait88@gmail.com>
2018-03-12 17:05:14 +0700

    update ngày 12/03

--- Barista Coffee/js/custom.js
+++ Barista Coffee/js/custom.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	  after: function(){}     // Function: After callback
 	});
 });
-
+// Back to top
 $(document).ready(function(){
 	$(window).scroll(function(){
         if($(this).scrollTop() > 600){
@@ -32,4 +32,47 @@ $(document).ready(function(){
 	});
 });
 
+
+// validate form
+
+$(document).ready(function(){
+	$('#register').on('submit', function(){
+		var isValid = true;
+		if($('#username').val().match(/(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/)== null){
+			$('#username').next('.error').text('Name is invalid');
+			isValid = false;
+		}else{
+			$('#username').next('.error').text('');
+		}
+
+		if($('#email').val().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/)== null){
+			$('#email').next('.error').text('Email is invalid');
+			isValid = false;
+		}else{
+			$('#email').next('.error').text('');
+		}
+
+		if($('#phone').val().match(/^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/)== null){
+			$('#phone').next('.error').text('Phone is invalid');
+			isValid = false;
+		}else{
+			$('#phone').next('.error').text('');
+		}
+
+		if($('#password').val().match(/^([a-zA-Z0-9@*#]{8,100})$/)== null){
+			$('#password').next('.error').text('password is invalid');
+			isValid = false;
+		}else{
+			$('#password').next('.error').text('');
+		}
+
+		if($('#date').val().match(/^((\d{2}(([02468][048])|([13579][26]))[\-\/\s]?((((0?[13578])|(1[02]))[\-\/\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\-\/\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\-\/\s]?((0?[1-9])|([1-2][0-9])))))|(\d{2}(([02468][1235679])|([13579][01345789]))[\-\/\s]?((((0?[13578])|(1[02]))[\-\/\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\-\/\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\-\/\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\s(((0?[1-9])|(1[0-2]))\:([0-5][0-9])((\s)|(\:([0-5][0-9])\s))([AM|PM|am|pm]{2,2})))?$/)== null){
+			$('#date').next('.error').text('date is invalid');
+			isValid = false;
+		}else{
+			$('#date').next('.error').text('');
+		}
+		return isValid;
+	});
+});
 
